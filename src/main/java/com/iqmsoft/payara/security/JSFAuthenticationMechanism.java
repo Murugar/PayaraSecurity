ffa83cb7c821cc25547251baa3bff267697d501e
Davanon <davanon2014@gmail.com>
2017-09-13 12:53:22 +0530

    First

---
+++ src/main/java/com/iqmsoft/payara/security/JSFAuthenticationMechanism.java
@@ -0,0 +1,69 @@
+
+package com.iqmsoft.payara.security;
+
+import javax.annotation.security.DeclareRoles;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.security.auth.message.AuthException;
+import javax.security.auth.message.AuthStatus;
+import javax.security.authentication.mechanism.http.HttpAuthenticationMechanism;
+import javax.security.authentication.mechanism.http.HttpMessageContext;
+import javax.security.authentication.mechanism.http.annotation.AutoApplySession;
+import javax.security.identitystore.CredentialValidationResult;
+import static javax.security.identitystore.CredentialValidationResult.Status.VALID;
+import javax.security.identitystore.IdentityStore;
+import javax.security.identitystore.annotation.Credentials;
+import javax.security.identitystore.annotation.EmbeddedIdentityStoreDefinition;
+import javax.security.identitystore.credential.Password;
+import javax.security.identitystore.credential.UsernamePasswordCredential;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@EmbeddedIdentityStoreDefinition({
+    @Credentials(callerName = "test", password = "test", groups = {"foo", "bar"}),
+    @Credentials(callerName = "test1", password = "test1", groups = {"foo", "kaz"}),
+    @Credentials(callerName = "test2", password = "test2", groups = {"foo"}),
+    @Credentials(callerName = "test3", password = "test3", groups = {"bar"})}
+)
+@DeclareRoles({"foo", "bar", "kaz"})
+@AutoApplySession
+@RequestScoped
+public class JSFAuthenticationMechanism implements HttpAuthenticationMechanism {
+
+    @Inject
+    private IdentityStore identityStore;
+
+    @Override
+    public AuthStatus validateRequest(HttpServletRequest request, HttpServletResponse response, HttpMessageContext httpMessageContext) throws AuthException {
+
+        if (request.getParameter("j_idt6:j_idt9") != null && request.getParameter("j_idt6:j_idt11") != null) {
+
+            // Get the (caller) name and password from the request
+            // NOTE: This is for the smallest possible example only. In practice
+            // putting the password in a request query parameter is highly
+            // insecure
+            String name = request.getParameter("j_idt6:j_idt9");
+            Password password = new Password(request.getParameter("j_idt6:j_idt11"));
+
+            // Delegate the {credentials in -> identity data out} function to
+            // the Identity Store
+            CredentialValidationResult result = identityStore.validate(
+                    new UsernamePasswordCredential(name, password));
+
+            if (result.getStatus() == VALID) {
+                // Communicate the details of the authenticated user to the
+                // container. In many cases the underlying handler will just store the details 
+                // and the container will actually handle the login after we return from 
+                // this method.
+                return httpMessageContext.notifyContainerAboutLogin(
+                        result.getCallerPrincipal(), result.getCallerGroups());
+            } else {
+                
+                return httpMessageContext.responseUnAuthorized();
+            }
+        }
+
+        return httpMessageContext.doNothing();
+    }
+
+}
---
+++ src/main/java/com/iqmsoft/payara/security/LoginBean.java
@@ -0,0 +1,40 @@
+
+package com.iqmsoft.payara.security;
+
+import java.io.IOException;
+import javax.enterprise.context.RequestScoped;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+import javax.inject.Named;
+
+
+@Named
+@RequestScoped
+public class LoginBean {
+
+    private String username;
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void login() throws IOException {
+
+        ExternalContext externalContext = FacesContext.getCurrentInstance().getExternalContext();
+        externalContext.redirect("hello.jsf");
+    }
+
+}
